e1960263a244c0ab145a3bda0ff23877ca5b5ad3
david paz brea <dpaz@lambda30.aulas.gsyc.es>
2015-01-26 14:47:38 +0100

    Añadido parte de otraSuma()

--- sumador.js
+++ sumador.js
@@ -6,4 +6,20 @@ function sumador () {
 	
 	var element = document.getElementById("res");
 	element.innerHTML = res;
+	
+	document.getElementById("Enlace").innerHTML = ""; 
+}
+
+function otraSuma(){
+	var operation = document.getElementById("op");
+	
+	op1 = getRandomArbitrary(-1000,1000);
+	op2 = getRandomArbitrary(-1000,1000);
+	cadena = op1+"+"+op2;
+	operation.innerHTml = cadena;
+	document.getElementById("Enlace").innerHTML = "Add!"; 
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
 }
